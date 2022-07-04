428f08eb3efdd3b4cec19fd585a5d98a5454d602
cowhca <connorwhcallahan@gmail.com>
2022-07-03 18:12:15 -0700

    Change listen url to fit work with Overcast

--- src/components/Idea.svelte
+++ src/components/Idea.svelte
@@ -15,7 +15,7 @@
 	}
 </script>
 
-<a href={idea.url + '?t=' + idea.start} target="_blank">
+<a href={idea.url + '/' + secondsToTimestamp(idea.start)} target="_blank">
 	<div
 		class="shadow-md shadow-slate-900  border-slate-500 bg-lime-200 p-2 hover:bg-lime-300 transition"
 	>
